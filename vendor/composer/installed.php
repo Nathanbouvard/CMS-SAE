c674a6b6bdb0037d4fec40b8f431c23c65104045
Nathan Bouvard <nathanbouvard76@gmail.com>
2026-01-19 13:02:39 +0100

    route + the + image taille

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cf805a4235b37251d1e7f092622e0ced9e32154e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cf805a4235b37251d1e7f092622e0ced9e32154e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'league/csv' => array(
+            'pretty_version' => '9.28.0',
+            'version' => '9.28.0.0',
+            'reference' => '6582ace29ae09ba5b07049d40ea13eb19c8b5073',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/csv',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
