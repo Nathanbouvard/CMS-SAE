f129e68c92a9d26a38a94b8914534e4e3935b150
victor <vicraveau@gmail.com>
2026-01-22 20:12:43 +0100

    mode mobile pour la nav barre

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -29,23 +29,32 @@ function Navbar() {
     window.location.href = '/';
   };
 
+  const closeMenu = () => {
+    const toggle = document.getElementById('nav-toggle');
+    if (toggle) toggle.checked = false;
+  };
+
   return (
     <nav className="navbar">
-        <div className="navbar-brand" style={{display: 'flex', alignItems: 'center'}}>
-            <img src={Logo} style={{width: '50px', height: '50px'}}/>
-            <div>Soul N Leaf</div>
-        </div>
+      <div className="navbar-brand" style={{display: 'flex', alignItems: 'center'}}>
+        <img src={Logo} style={{width: '50px', height: '50px'}} alt="Logo"/>
+        <div>Soul N Leaf</div>
+      </div>
+      
+      <input type="checkbox" id="nav-toggle" className="nav-toggle" />
+      <label htmlFor="nav-toggle" className="mobile-menu-icon">&#9776;</label>
+
       <ul className="navbar-nav">
-        <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/boutique">Boutique</Link></li>
-        <li><Link to="/blog">Actualités</Link></li>
+        <li><Link to="/" onClick={closeMenu}>Accueil</Link></li>
+        <li><Link to="/boutique" onClick={closeMenu}>Boutique</Link></li>
+        <li><Link to="/blog" onClick={closeMenu}>Actualités</Link></li>
         {isAdmin && (
-            <li><a href={`http://localhost:8000/connect-admin?token=${localStorage.getItem('token')}`} className="nav-link admin-link">Administration</a></li>
+          <li><a href={`http://localhost:8000/connect-admin?token=${localStorage.getItem('token')}`} className="nav-link admin-link" onClick={closeMenu}>Administration</a></li>
         )}
         {isAuthenticated ? (
-            <li><a href="/" onClick={handleLogout} className="btn-logout">Déconnexion</a></li>
+          <li><a href="/" onClick={handleLogout} className="btn-logout">Déconnexion</a></li>
         ) : (
-            <li><Link to="/login" className="btn-login">Connexion</Link></li>
+          <li><Link to="/login" className="btn-login" onClick={closeMenu}>Connexion</Link></li>
         )}
       </ul>
     </nav>
